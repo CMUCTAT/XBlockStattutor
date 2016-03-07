e49ad75d1a24696f996190f83f1f6afd97898fb0
Ringenberg <Ringenberg@users.noreply.github.com>
2016-03-07 15:21:26 -0500

    Removed bad confict marker.

--- stattutor/stattutor.py
+++ stattutor/stattutor.py
@@ -71,7 +71,6 @@ class StattutorXBlock(XBlock):
     log_level2 = String(help="Level name to log", default="unit2", scope=Scope.settings)
     log_type2 = String(help="Level type to log", default="unit", scope=Scope.settings)
     log_url = String(help="URL of the logging service", default="http://pslc-qa.andrew.cmu.edu/log/server", scope=Scope.settings)
->>>>>>> master
     logtype = String(help="How should data be logged", default="clienttologserver", scope=Scope.settings)
     log_diskdir = String(help="Directory for log files relative to the tutoring service", default=".", scope=Scope.settings)
     log_port = String(help="Port used by the tutoring service", default="8080", scope=Scope.settings)
