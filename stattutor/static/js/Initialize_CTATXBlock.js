a277e441a7b9032c6cb617fdb685fed2b9cc3e92
Ringenberg <Ringenberg@users.noreply.github.com>
2016-09-21 13:09:18 -0400

    Fixed brd loading and linked to correct xml2json library.

--- stattutor/static/js/Initialize_CTATXBlock.js
+++ stattutor/static/js/Initialize_CTATXBlock.js
@@ -26,21 +26,21 @@ function Initialize_CTATXBlock(runtime,element) {
 		    contentType: "application/json; charset=utf-8",
 		    dataType: "json"});
 	},
-	log_event: function(aMessage) 
+	log_event: function(aMessage)
 	{
 		//console.log ("log_event :" + aMessage);
-		
+
 	    $.ajax({type: "POST",
-				url: runtime.handlerUrl(element, 'ctat_log'),
-				data: JSON.stringify({'event_type':'ctat_log',
-					   'action':'logevent', 
-					   'message': aMessage}),
-				contentType: "application/json; charset=utf-8",
-				dataType: "json"
-			   });
-	}	
+		    url: runtime.handlerUrl(element, 'ctat_log'),
+		    data: JSON.stringify({'event_type':'ctat_log',
+					  'action':'logevent',
+					  'message': aMessage}),
+		    contentType: "application/json; charset=utf-8",
+		    dataType: "json"
+		   });
+	}
     };
-    $('.stattutor').load(function() { // this is getting fired after initTutor
+    $('.stattutor').on("load", function() { // this is getting fired after initTutor
 	this.contentWindow.CTATTarget = "XBlock"; // needed for ctatloader.js
 	var lms = this.contentWindow.CTATLMS;
 	lms.identifier = 'XBlock';
@@ -58,10 +58,10 @@ function Initialize_CTATXBlock(runtime,element) {
 	lms.gradeStudent = function (correct_step_count, total_step_count) {
 	    post.report_grade(correct_step_count, total_step_count);
 	};
-	lms.logEvent = function (aMessage) 
+	lms.logEvent = function (aMessage)
 	{
 	    post.log_event(aMessage);
-	};	
+	};
 	// CTATConfig is from CTATConfig.js which is a template that is filled
 	// out by ctatxblock.py
 	this.contentWindow.initialize_stattutor(CTATConfig);
